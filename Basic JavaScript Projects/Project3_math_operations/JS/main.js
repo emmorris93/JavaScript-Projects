a87d21f8da5da9be24ee030b8bb01aadb567f2fc
emmorris93 <maudi.morris@gmail.com>
2020-11-30 13:56:29 -0800

    Math Function Assignments
    added math function assignments

---
+++ Basic JavaScript Projects/Project3_math_operations/JS/main.js
@@ -0,0 +1,45 @@
+function addition_Function() {                                      //addition function defined and named
+    var addition = 5 + 2;                                           //variable defined 
+    document.getElementById("Math1").innerHTML = addition;          //result value into HTML element by id
+}
+
+function subtraction_Function() {                                   //subtraction function defined and named
+    var subtraction = 7 - 5;
+    document.getElementById("Math2").innerHTML = subtraction
+}
+
+function multiplication_Function() {                                //multiplication function defined and named
+    var multiplication = 5 * 2;
+    document.getElementById("Math3").innerHTML = multiplication     
+}
+
+function division_Function() {                                      //division function defined and named
+    var division = 45 / 5;
+    document.getElementById("Math4").innerHTML = division           
+}
+function multiple_Functions() {                                     //addition function defined and named
+    var multiple_math = (5 + 2) * 45 / 5 -5;
+    document.getElementById("Math5").innerHTML = multiple_math
+}
+function modulus_Operator() {                                       //modulus operator function defined and named
+    var modulus = 55 % 2;
+    document.getElementById("Math6").innerHTML = modulus
+}
+
+function negation_Operator() {                                      //negation operator function defined and named
+    var negation = 1995;
+    document.getElementById("Math7").innerHTML = negation
+}
+
+function increment_Operator() {                                     //increment operator function defined and named
+    var X = 1995;
+    X++;
+    document.getElementById("Math8").innerHTML = X
+}
+
+function decrement_Operator() {                                     //decrement operator function defined and named
+    var X = 7.5;
+    X--;
+    document.getElementById("Math9").innerHTML = X
+}
+
