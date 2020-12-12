55f6c6af4d73d160870b4c918fe4b1eed0856465
emmorris93 <maudi.morris@gmail.com>
2020-12-11 16:48:57 -0800

    Added HTML_4, React_3 & Updated HTML_3
    added HTML_4, React_3 and udpated HTML_3

--- JavaScript Projects/JavaScript/JavaScript_3.js
+++ JavaScript Projects/JavaScript/JavaScript_3.js
@@ -4,3 +4,8 @@ function displayType(character) {
     var characterType = character.getAttribute("data-character-type");
     alert("I play the character " + characterType + " in the MMORPG, " + character.innerHTML + ".")
 }
+
+// CHALLENGE (Step 360)
+$(function() {
+    $('body').removeClass('fade-out');
+});
